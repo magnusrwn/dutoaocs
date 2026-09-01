f881c8bbace2f430b9779de6607ae45f04db5b13
magnusrwn <119822862+magnusrwn@users.noreply.github.com>
2026-09-01 22:27:03 +0100

    fixing typos and removing useless lines

--- app/src/controllers/addLlmLink.ts
+++ app/src/controllers/addLlmLink.ts
@@ -1,5 +1,5 @@
 import { Project } from "../entities/index";
-import { getProjectIfExists, showApiLinksteps, verifyDotenv } from "../use-cases/add-llm/index";
+import { getProjectIfExists, showApiLinksteps } from "../use-cases/add-llm/index";
 
 
 export default async function addLlmLink(userLocation: string){
@@ -9,8 +9,10 @@ export default async function addLlmLink(userLocation: string){
         console.log("No 'dutoaocs.config.json' was found. Make sure you are at the project root and/ or run 'dutoaocs init' to create one")
         return
     }
+
     // show linnking steps
     showApiLinksteps()
+    
+    // cap steps with final console log directing to next step
     console.log("\nOnce complete, run 'dutoaocs add-llm --verify'")
-    return
 }
--- app/src/use-cases/add-llm/verifyGitignore.ts
+++ app/src/use-cases/add-llm/verifyGitignore.ts
@@ -3,7 +3,7 @@ import rollbackDotenv from "./rollbackDotenv"
 
 export default function verifyGitignore(currentpath:string):boolean{
     if (!fs.existsSync(currentpath+".gitignore")){
-        console.log("Can not fund .gitignore")
+        console.log("Can not find .gitignore")
         console.log("Make sure '.gitignore' is in your project root")
         console.log("Make sure you are located in your project root ")
         return false
@@ -12,11 +12,11 @@ export default function verifyGitignore(currentpath:string):boolean{
     // read gitignore to ensure '.env' is in it
     const gitignore_data:string = fs.readFileSync(currentpath+".gitignore", "utf-8")
     if (!gitignore_data.includes(".env")){
-        console.log("'.env' not found in your .gitignore")
-        console.log("It necessary for it to be in there to continue")
-        console.log("Rolling back .env change. Re-add your OpenAi API key again, and re-run 'dutoaocs add-llm --verify'")
+        console.log("'.env' not found inside your .gitignore")
+        console.log("It is necessary for it to be in there to continue")
+        console.log("Rolling back your change in your .env.")
         rollbackDotenv()
-        
+        console.log("Re-add your OpenAi API key again, and re-run 'dutoaocs add-llm --verify'")
         return false
     }
     return true
